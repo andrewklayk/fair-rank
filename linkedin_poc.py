461f11f19b85c7f010747db8d761c3eba2e62bde
andrewklayk <andrew.klyachkin@gmail.com>
2023-05-16 12:13:05 +0200

    fixed alg, added test data generator

--- linkedin_poc.py
+++ linkedin_poc.py
@@ -2,19 +2,29 @@ import numpy as np
 import pandas as pd
 
 
-def detconstsort(items: pd.DataFrame | dict, sens_attr: str, sens_vals: list,  p: list, kmax: int):
+def detconstsort(items: pd.DataFrame | dict, sens_attr: str, 
+                 probs: dict, kmax: int=10,):
     rankedAttList = []
     rankedScoreList = []
     rankedItems = []
     maxIndices = []
+    sens_vals = list(probs.keys())
+    p = list(probs.values())
     counts = np.zeros(shape=len(sens_vals), dtype=int)
     min_counts = np.zeros(shape=len(sens_vals), dtype=int).tolist()
     lastEmpty = 0
     k=0
+    # check if there's enough items of each class to satisfy constraint
+    for sv in probs.keys():
+        target_count = kmax*probs[sv]
+        if target_count > items[items[sens_attr] == sv].shape[0]:
+            raise ValueError(
+                f'Not enough items of group -- {sv} -- to satisfy target constraint!'
+            )
     # group the items by their sensitive attribute values
     if not isinstance(sens_vals, dict):
-        items = items.sort_values(axis=0, by=sens_attr)
-        items_by_a = [it for ai, it in zip(sens_vals, [items[items[sens_attr] == ai] for ai in sens_vals])]
+        items = items.sort_values(axis=0, by='score',ascending=False)
+        item_groups = [it for ai, it in zip(sens_vals, [items[items[sens_attr] == ai] for ai in sens_vals])]
     else:
         # check if dict is constructed correctly
         raise(NotImplementedError)
@@ -35,7 +45,7 @@ def detconstsort(items: pd.DataFrame | dict, sens_attr: str, sens_vals: list,  p
             # get the list of candidates to insert and sort them by their score
             changed_items = []
             for i in changed_mins:
-                changed_items.append(items_by_a[i].iloc[counts[i]])
+                changed_items.append(item_groups[i].iloc[counts[i]])
             changed_items.sort(key=lambda x: x['score'])
 
             # add the items, starting with the best score
@@ -54,23 +64,30 @@ def detconstsort(items: pd.DataFrame | dict, sens_attr: str, sens_vals: list,  p
                 lastEmpty+=1
                 counts[sens_vals.index(newitem[sens_attr])] += 1
             min_counts = min_counts_at_k
-    return rankedItems, rankedScoreList, rankedAttList
+    return rankedItems
+
+def gen_fake_data(sens_attr: str, sens_prop: dict, score_means: dict, k: int=100, seed: float=None):
+    groups = []
+    for ai in sens_prop.keys():
+        df_i = pd.DataFrame(columns=['score', sens_attr])
+        pi = sens_prop[ai]
+        mi = score_means[ai]
+        ki = int(k*pi)
+        scores_i = np.random.default_rng(seed=seed).poisson(lam=mi, size=ki)
+        df_i['score'] = scores_i
+        df_i[sens_attr] = [ai]*ki
+        groups.append(df_i)
+    return pd.concat(groups)
+
+def infeasible_index(ranking, probs):
+    raise NotImplementedError
 
 
 def main():
     inp = pd.DataFrame(columns = ['a', 'score'])
-    inp.loc[0] = ['blue', 56]
-    inp.loc[1] = ['blue', 55]
-    inp.loc[2] = ['blue', 45]
-    inp.loc[3] = ['red', 44]
-    inp.loc[4] = ['red', 44]
-    inp.loc[5] = ['blue', 25]
-    inp.loc[6] = ['blue', 23]
-    inp.loc[7] = ['blue', 18]
-    inp.loc[8] = ['red', 16]
-    inp.loc[9] = ['red', 15]
-    inp.loc[10] = ['red', 15]
-    res = detconstsort(sens_vals = ['red', 'blue'], items = inp, p = [0.7, 0.3], kmax = 6, sens_attr = 'a')
+    df = gen_fake_data('a', {'blue': 0.7, 'red':0.3}, {'blue': 70, 'red': 40}, k=100, seed=42)
+    df.sort_values(by='score', inplace=True, ascending=False)
+    res = detconstsort(items = df, probs = {'red': 0.5, 'blue': 0.5}, kmax = 10, sens_attr = 'a')
     print(pd.DataFrame(res[0]))
 
 if __name__ == "__main__":
